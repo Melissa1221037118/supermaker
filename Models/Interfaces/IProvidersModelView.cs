89721c2776921d413921ee4318843ace752c8d40
Melissa1221037118 <melissavallecilla0821@gmail.com>
2023-10-21 02:50:15 -0500

    interfaz de repositorio providers

---
+++ Models/Interfaces/IProvidersModelView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermarket.Models.Interfaces
+{
+    internal interface IProvidersModelView
+    {
+        void Add(PayModeModel payModelModel);
+        void Edit(PayModeModel payModelModel);
+        void Delete(int id);
+
+        IEnumerable<PayModeModel> GetAll();
+        IEnumerable<PayModeModel> GetByValue(string value);
+    }
+}
