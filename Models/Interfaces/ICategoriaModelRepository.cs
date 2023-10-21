cec7c9b70d75a4afea165d6afda228eb4ab501e9
Melissa1221037118 <melissavallecilla0821@gmail.com>
2023-10-21 01:51:19 -0500

    interfaz del repositorio categorias creada

---
+++ Models/Interfaces/ICategoriaModelRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermarket.Models.Interfaces
+{
+    internal interface ICategoriaModelRepository
+    {
+        void Add(CategoriasModel categoriaModel);
+        void Edit(CategoriasModel categoriaModel);
+        void Delete(int id);
+
+        IEnumerable<CategoriasModel> GetAll();
+        IEnumerable<CategoriasModel> GetByValue(string value);
+    }
+}
