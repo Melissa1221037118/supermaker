ae173cb286c4f1beb0c8a956bf8f3b29fa283b76
Melissa1221037118 <melissavallecilla0821@gmail.com>
2023-10-21 01:38:29 -0500

    view producto creada

---
+++ Views/ProductView.cs
@@ -0,0 +1,187 @@
+﻿using supermarket.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace supermarket.Views
+{
+    public partial class ProductView : Form, IProductModelView
+    {
+        private bool isEdit;
+        private bool isSuccessful;
+        private string message;
+
+        public ProductView()
+        {
+            InitializeComponent();
+            AssociateAndRaiseViewEvents();
+
+            btnClose.Click += delegate { this.Close(); };
+        }
+
+        
+
+        public bool IsEdit
+        {
+            get { return isEdit; }
+            set { isEdit = value; }
+        }
+        public bool IsSuccessful
+        {
+            get { return isSuccessful; }
+            set { isSuccessful = value; }
+        }
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        public string ProductModelId
+        {
+            get { return tabControl1.Text; }
+            set { tabControl1.Text = value; }
+        }
+
+        public string ProductModeName
+        {
+            get { return TxtPayModeName.Text; }
+            set { TxtPayModeName.Text = value; }
+        }
+
+        public string ProductModePrice
+        {
+            get { return TxtPayModeObservation.Text; }
+            set { TxtPayModeObservation.Text = value; }
+        }
+
+        public string ProductModeStock
+        {
+            get { return TxtPayModeObservation.Text; }
+            set { TxtPayModeObservation.Text = value; }
+        }
+
+        public string ProductModeCategoryId
+        {
+            get { return TxtPayModeObservation.Text; }
+            set { TxtPayModeObservation.Text = value; }
+        }
+
+        public string SearchValue
+        {
+            get { return TxtSearch.Text; }
+            set { TxtSearch.Text = value; }
+        }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+
+        public void SetProductModeListBildingSource(BindingSource payModeList)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AssociateAndRaiseViewEvents()
+        {
+            btnSearch.Click += delegate { SearchEvent?.Invoke(this, EventArgs.Empty); };
+            TxtSearch.KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                    SearchEvent?.Invoke(this, EventArgs.Empty);
+            };
+            btnsave.Click += delegate
+        {
+            AddNewEvent?.Invoke(this, EventArgs.Empty);
+
+            tabControl1.TabPages.Remove(tabPageProductList);
+            tabControl1.TabPages.Add(tabPageProductDetail);
+            tabPageProductDetail.Text = "Add new Product";
+        };
+
+            btnEdit.Click += delegate
+            {
+                EditEvent?.Invoke(this, EventArgs.Empty);
+
+                tabControl1.TabPages.Remove(tabPageProductList);
+                tabControl1.TabPages.Add(tabPageProductDetail);
+                tabPageProductDetail.Text = "Edit Product Mode";
+            };
+            btnDelet.Click += delegate
+            {
+
+                var result = MessageBox.Show(
+                    "Are sure you want to delete the selected Pay Mode",
+                    "Warning",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    DeleteEvent?.Invoke(this, EventArgs.Empty);
+                    MessageBox.Show(message);
+                }
+            };
+            btnsave.Click += delegate
+            {
+                SaveEvent?.Invoke(this, EventArgs.Empty);
+
+                if (isSuccessful)
+                {
+                    tabControl1.TabPages.Remove(tabPageProductDetail);
+                    tabControl1.TabPages.Add(tabPageProductList);
+                }
+                MessageBox.Show(message);
+            };
+            btnCancel.Click += delegate
+            {
+                CancelEvent?.Invoke(this, EventArgs.Empty);
+
+                tabControl1.TabPages.Remove(tabPageProductDetail);
+                tabControl1.TabPages.Add(tabPageProductList);
+            };
+        }
+
+        private void BtnSearch_Click(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPayModeListBildingSource(BindingSource payModeList)
+        {
+            DgProduct.DataSource = payModeList;
+        }
+
+        private static PayModeView instance;
+
+
+        public static PayModeView GetInstance(Form parentContainer)
+        {
+            if (instance == null || instance.IsDisposed)
+            {
+                instance = new PayModeView();
+                instance.MdiParent = parentContainer;
+
+                instance.FormBorderStyle = FormBorderStyle.None;
+                instance.Dock = DockStyle.Fill;
+            }
+            else
+            {
+                if (instance.WindowState == FormWindowState.Minimized)
+                {
+                    instance.WindowState = FormWindowState.Normal;
+                }
+                instance.BringToFront();
+            }
+            return instance;
+        }
+    }
+}
