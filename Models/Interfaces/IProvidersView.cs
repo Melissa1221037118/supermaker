2e02e576e904b69a6ef833365ce48a77a0977ab1
Melissa1221037118 <melissavallecilla0821@gmail.com>
2023-10-21 02:52:33 -0500

    interfaz de la vista de providers creada

---
+++ Models/Interfaces/IProvidersView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermarket.Models.Interfaces
+{
+    internal interface IProvidersView
+    {
+        string ProvierlId { get; set; }
+        string ProvierName { get; set; }
+        string ProvierDescription { get; set; }
+
+        string SearchValue { get; set; }
+        bool IsEdit { get; set; }
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+
+        void SetProvidersListBildingSource(BindingSource payModeList);
+
+        void Show();
+
+    }
+}
