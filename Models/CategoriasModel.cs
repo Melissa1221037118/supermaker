fc71d5fc06fcaa587cad6c9404fb833618f79316
Melissa1221037118 <melissavallecilla0821@gmail.com>
2023-10-21 01:47:58 -0500

    modelo categiras creada

---
+++ Models/CategoriasModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace supermarket.Models
+{
+    internal class CategoriasModel
+    {
+        [DisplayName("Categoria Id")]
+        public int id { get; set; }
+        [DisplayName("Categoria Name")]
+        [Required(ErrorMessage = "Categoria name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Categoria name must be between 3 and 50 characters")]
+        public string name { get; set; }
+
+        [DisplayName("Product Name")]
+        [Required(ErrorMessage = "Categoria description is required")]
+        public string description { get; set; }
+    }
+}
